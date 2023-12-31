4015cfdd9d508583397b6bdc8d47f685ec7de308
ZAMAN MONIR <monir.cse6.bu@gmail.com>
2023-12-31 14:47:00 +0600

    footer

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <div className="bg-gray-900 ">
-      <div className="max-w-2xl md:mx-auto text-white py-10">
-        <div className="md:text-center text-left">
+      <div className="max-w-2xl sm:mx-auto text-white py-10">
+        <div className="sm:text-center text-left">
           <h3 className="text-3xl mb-3">Download our app</h3>
           <p>Stay connected All day, every day.</p>
-          <div className="md:flex justify-center my-10">
+          <div className="sm:flex justify-center my-10">
             <div className="flex items-center border  rounded-lg px-4 py-2 w-52 mx-2">
               <img
                 src="https://cdn-icons-png.flaticon.com/512/888/888857.png"
@@ -20,7 +20,7 @@ const Footer = () => {
                 <p className="text-sm md:text-base">Google Play Store</p>
               </div>
             </div>
-            <div className="md:flex items-center border md:w-auto w-52 rounded-lg px-4 py-2  mx-2">
+            <div className="sm:flex items-center border sm:w-auto w-52 rounded-lg px-4 py-2  mx-2">
               <img
                 src="https://cdn-icons-png.flaticon.com/512/888/888841.png"
                 className="w-7 md:w-8"
@@ -33,9 +33,9 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="mt-28 md:flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400 text-left">
-          <p className="order-2 md:order-1 mt-8 md:mt-0">&copy; Niye Jao, 2023.</p>
-          <div className="order-1 md:order-2">
+        <div className="mt-28 sm:flex flex-col sm:flex-row sm:justify-between items-center text-sm text-gray-400 text-left">
+          <p className="order-2 sm:order-1 mt-8 md:mt-0">&copy; Niye Jao, 2023.</p>
+          <div className="order-1 sm:order-2">
            <Link to='/help'> <span className="px-2">About us</span></Link>
            <Link to='/help'> <span className="px-2">Contact us</span></Link>
            <Link to='/policy'> <span className="px-2">Privacy Policy</span></Link>   
